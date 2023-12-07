27cc2034a4248466655aab0f15dd75e8f7f1920e
derickfelix <derickfelix@zoho.com>
2023-12-07 21:12:40 +0000

    create exercise 01

--- src/main/java/com/github/beacastroalves/chapter1/Ex01.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex01.java
@@ -1,3 +1,11 @@
+/**
+ * Write a program that prompts a first name and an age, then display the
+ * name the user entered with the following age in months.
+ * e.g:
+ * $ Please enter your first name and age:
+ * > John 23
+ * "Hello, John Doe (age 276 months).
+ */
 package com.github.beacastroalves.chapter1;
 
 public class Ex01 {
