9ab4d440a0452d552c203f813e73e81f57498fe3
beatriz alves <beacastro1008@gmail.com>
2023-12-09 15:22:11 +0000

    solve exercice 04

--- src/main/java/com/github/beacastroalves/chapter1/Ex04.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex04.java
@@ -1,9 +1,32 @@
 /**
- * Modify the previous program to ask the user to enter floating-point values and
+ * Modify the previous program to ask the user to enter floating-point values
  * and store them in double variables.
  */
 package com.github.beacastroalves.chapter1;
 
-public class Ex04 {
+import java.util.Scanner;
 
+public class Ex04 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Type the first number:");
+    double val1 = input.nextDouble();
+    System.out.println("Type the second number:");
+    double val2 = input.nextDouble();
+    double smaller = val1;
+    double larger = val2;
+
+    if (val1 > val2) {
+      smaller = val2;
+      larger = val1;
+    }
+
+    System.out.println("The smaller: " + smaller);
+    System.out.println("The larger: " + larger);
+    System.out.println("sum: " + (val1 + val2));
+    System.out.println("difference: " + (val1 - val2));
+    System.out.println("product: " + (val2 * val1));
+    System.out.println("ratio: " + (val1 / val2));
+  }
 }
+
