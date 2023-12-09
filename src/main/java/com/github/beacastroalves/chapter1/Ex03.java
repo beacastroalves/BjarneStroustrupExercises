5662ed44f6db5756fc6484486af481fac3d5be5d
beatriz alves <beacastro1008@gmail.com>
2023-12-09 14:52:48 +0000

    solve exercise 03

--- src/main/java/com/github/beacastroalves/chapter1/Ex03.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex03.java
@@ -6,6 +6,28 @@
  */
 package com.github.beacastroalves.chapter1;
 
-public class Ex03 {
+import java.util.Scanner;
 
+public class Ex03 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Type the first number:");
+    int val1 = input.nextInt();
+    System.out.println("Type the second number:");
+    int val2 = input.nextInt();
+    int smaller = val1;
+    int larger = val2;
+
+    if (val1 > val2) {
+      smaller = val2;
+      larger = val1;
+    }
+
+    System.out.println("The smaller: " + smaller);
+    System.out.println("The larger: " + larger);
+    System.out.println("sum: " + (val1 + val2));
+    System.out.println("difference: " + (val1 - val2));
+    System.out.println("product: " + (val2 * val1));
+    System.out.println("ratio: " + (val1 / val2));
+  }
 }
