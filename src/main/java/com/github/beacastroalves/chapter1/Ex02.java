1e6cf75f1308aac90dbaf614685dc4a150d3feee
beatriz alves <beacastro1008@gmail.com>
2023-12-09 13:29:31 +0000

    solve exercise 02

--- src/main/java/com/github/beacastroalves/chapter1/Ex02.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex02.java
@@ -4,6 +4,25 @@
  */
 package com.github.beacastroalves.chapter1;
 
-public class Ex02 {
+import java.util.Scanner;
 
+public class Ex02 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("To convert miles to kilometers. Enter how many miles you want");
+    double dMi = input.nextDouble();
+    double dKm = Double.parseDouble(String.format("%f", dMi * 1.609344));
+    String mileWord = "mile is";
+    String kilometerWord = "kilometer";
+
+    if (dMi > 1.0) {
+      mileWord = "miles are";
+    }
+
+    if (dKm > 1.0) {
+      kilometerWord = "kilometers";
+    }
+
+    System.out.printf("%.2f %s %f %s", dMi, mileWord, dKm, kilometerWord);
+  }
 }
