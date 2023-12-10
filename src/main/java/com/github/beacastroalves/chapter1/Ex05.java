553a93b05266258c80cf924d68d73aef520024bd
beatriz alves <beacastro1008@gmail.com>
2023-12-10 23:21:23 +0000

    solve exercise 05

--- src/main/java/com/github/beacastroalves/chapter1/Ex05.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex05.java
@@ -7,6 +7,55 @@
  */
 package com.github.beacastroalves.chapter1;
 
-public class Ex05 {
+import java.util.Scanner;
 
+public class Ex05 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Type three numbers:");
+    int val1 = input.nextInt();
+    int val2 = input.nextInt();
+    int val3 = input.nextInt();
+
+    int first;
+    int second;
+    int third;
+
+    if (val1 <= val2 && val1 <= val3) {
+      first = val1;
+
+      if (val2 < val3) {
+        second = val2;
+        third = val3;
+      } else {
+        second = val3;
+        third = val2;
+      }
+    } else if (val2 < val1 && val2 <= val3) {
+      first = val2;
+
+      if (val1 < val3) {
+        second = val1;
+        third = val3;
+      } else {
+        second = val3;
+        third = val1;
+      }
+    } else {
+      first = val3;
+
+      if (val1 < val2) {
+        second = val1;
+        third = val2;
+      } else {
+        second = val2;
+        third = val1;
+      }
+    }
+
+    String comma1 = first != second ? "," : "";
+    String comma2 = second != third ? "," : "";
+
+    System.out.printf("%d%s %d%s %d", first, comma1, second, comma2, third);
+  }
 }
