bbda52b1ebdf31a94cbaf3db84d582fb98df93f1
beatriz alves <beacastro1008@gmail.com>
2023-12-12 11:05:55 +0000

    solve exercise 08

--- src/main/java/com/github/beacastroalves/chapter1/Ex08.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex08.java
@@ -6,6 +6,34 @@
  */
 package com.github.beacastroalves.chapter1;
 
-public class Ex08 {
+import java.util.Scanner;
 
+public class Ex08 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Write a spelled-out number, such as one, two, three or four");
+    String spelledNumber = input.next().toUpperCase();
+    int value = 0;
+
+    switch (spelledNumber) {
+      case "ONE":
+        value = 1;
+        break;
+      case "TWO":
+        value = 2;
+        break;
+      case "THREE":
+        value = 3;
+        break;
+      case "FOUR":
+        value = 4;
+        break;
+      default:
+        System.out.println("Not a number I know");
+    }
+
+    if (value > 0) {
+      System.out.println(value);
+    }
+  }
 }
