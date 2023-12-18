72c4dc5cf7395d63f444dd5028e4b7e357e52d86
beatriz alves <beacastro1008@gmail.com>
2023-12-18 23:02:49 +0000

    solve exercise 09

--- src/main/java/com/github/beacastroalves/chapter1/Ex09.java
+++ src/main/java/com/github/beacastroalves/chapter1/Ex09.java
@@ -9,6 +9,65 @@
  */
 package com.github.beacastroalves.chapter1;
 
-public class Ex09 {
+import java.util.Scanner;
 
+public class Ex09 {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the calculator for all 4 operations. " +
+        "Write two operands in the same line. " +
+        "For example 100 + 3.14 ");
+
+
+    while (true) {
+      System.out.print("> ");
+      try {
+        Scanner input = new Scanner(System.in);
+        String term1 = input.next();
+
+        if (term1.equalsIgnoreCase("EXIT")) {
+          break;
+        }
+
+        String option = input.next();
+
+        if (option.equalsIgnoreCase("EXIT")) {
+          break;
+        }
+
+        String term2 = input.next();
+
+        if (term2.equalsIgnoreCase("EXIT")) {
+          break;
+        }
+
+        float n1 = Float.parseFloat(term1);
+        float n2 = Float.parseFloat(term2);
+        float result;
+
+        switch (option) {
+          case "+":
+            result = n1 + n2;
+            break;
+          case "-":
+            result = n1 - n2;
+            break;
+          case "*":
+            result = n1 * n2;
+            break;
+          case "/":
+            result = n1 / n2;
+            break;
+          default:
+            throw new Exception("Invalid Operation, try: +, -, * or /");
+        }
+
+        System.out.println(result);
+      } catch (NumberFormatException e) {
+        System.out.println("Failed to parse number " + e.getMessage());
+      } catch (Exception e) {
+        System.out.println("Failed to calculate: " + e.getMessage());
+      }
+    }
+  }
 }
+
